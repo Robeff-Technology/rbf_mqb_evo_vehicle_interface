70c7050bce3445b27aaefd588884bf1e16ccab64
Yunus Emre Elaydin <63052608+yunusemreelaydin@users.noreply.github.com>
2026-01-29 13:07:27 +0300

    fix QoS

--- robione_vehicle_interface/src/robione_vehicle_interface.cpp
+++ robione_vehicle_interface/src/robione_vehicle_interface.cpp
@@ -98,7 +98,8 @@ void RobioneVehicleInterface::init_subscribers()
         &RobioneVehicleInterface::vehicle_emergency_cmd_callback, this, std::placeholders::_1));
 
   sub_route_state_ = this->create_subscription<autoware_adapi_v1_msgs::msg::RouteState>(
-    "/api/ad_api_adapter/routing_state", rclcpp::QoS(1),
+    "/api/ad_api_adapter/routing_state",
+    rclcpp::QoS(rclcpp::KeepLast(1)).reliable().transient_local(),
     std::bind(&RobioneVehicleInterface::route_state_callback, this, std::placeholders::_1));
 
   tablet_feedback_sub_ = this->create_subscription<robeff_msgs::msg::TabletFeedback>(
